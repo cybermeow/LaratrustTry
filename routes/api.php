de77ff68a50cab41ba7517a74a03b6bdbbfcc1f1
Isidora Todosic <isidora.todosic@bitgear.rs>
2018-01-10 14:55:47 +0100

    manhole and alarmList models + controllers

--- routes/api.php
+++ routes/api.php
@@ -18,34 +18,34 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => 'auth:api'], function() {
-    Route::get('manhole/{manhole}', 'ManholeController@show');
+//    Route::get('manhole/{manhole}', 'ManholeController@show');
     Route::get('alarm-list', 'AlarmListController@index');
 
 });
 
 Route::get('manhole', 'ManholeController@index');
-Route::post('manhole', 'ManholeController@store');
-Route::put('manhole/{manhole}', 'ManholeController@update');
-Route::delete('manhole/{manhole}', 'ManholeController@delete');
+//Route::post('manhole', 'ManholeController@store');
+//Route::put('manhole/{manhole}', 'ManholeController@update');
+//Route::delete('manhole/{manhole}', 'ManholeController@delete');
 
 //Route::get('alarm-list', 'AlarmListController@index');
-Route::get('alarm-list/{alarmList}', 'AlarmListController@show');
-Route::post('alarm-list', 'AlarmListController@store');
-Route::put('alarm-list/{alarmList}', 'AlarmListController@update');
-Route::delete('alarm-list/{id_alarm}', 'AlarmListController@delete');
+//Route::get('alarm-list/{alarmList}', 'AlarmListController@show');
+//Route::post('alarm-list', 'AlarmListController@store');
+//Route::put('alarm-list/{alarmList}', 'AlarmListController@update');
+//Route::delete('alarm-list/{id_alarm}', 'AlarmListController@delete');
 
 
-Route::put('ack-alarm/{alarmList}', 'AckAlarmController@update');
+//Route::put('ack-alarm/{alarmList}', 'AckAlarmController@update');
+//
+//
+//Route::get('device', 'DeviceController@index');
+//Route::get('device/{device}', 'DeviceController@show');
+//Route::post('device', 'DeviceController@store');
+//Route::put('device/{device}', 'DeviceController@update');
+//Route::delete('device/{device}', 'DeviceController@delete');
 
 
-Route::get('device', 'DeviceController@index');
-Route::get('device/{device}', 'DeviceController@show');
-Route::post('device', 'DeviceController@store');
-Route::put('device/{device}', 'DeviceController@update');
-Route::delete('device/{device}', 'DeviceController@delete');
-
-
-Route::put('user/{user}', 'UserColController@update');
+//Route::put('user/{user}', 'UserColController@update');
 
 
 Route::post('login', 'Auth\LoginController@login');
